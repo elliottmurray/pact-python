9d1bb00c8bb4867cd51db354c2645932a094f0d6
Elliott Murray <elliottmurray@gmail.com>
2020-04-03 16:09:21 +0100

    Script now shuts down a bit better

--- examples/e2e/verify_pact.sh
+++ examples/e2e/verify_pact.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+set -o pipefail
+
+python provider.py & &>/dev/null
+FLASK_PID=$!
+
+echo $FLASK_PID
+
+quit() {
+    # some weirdness about where this process is running so have to get the child
+    echo "Kill $1"
+    CHILD=`pgrep -P $1`
+
+    kill -9 $CHILD
+    exit $?
+}
+
 VERSION=$1
 if [ -x $VERSION ]; then
     echo "ERROR: You must specify a provider version"
@@ -11,7 +27,9 @@ pact-verifier --provider-base-url=http://localhost:5001 \
   tests/userserviceclient-userservice.json
 
 
+quit $FLASK_PID
 
+# quit
   #   --pact-url="http://127.0.0.1/pacts/provider/UserService/consumer/UserServiceClient/latest" \
   # --provider-states-setup-url=http://localhost:5001/_pact/provider_states \
   #  --pact-broker-username pactbroker \
